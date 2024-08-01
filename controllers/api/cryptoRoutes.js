814c45ba9db24c78b0289a6c2078e7e3620922f6
wilsacker <williamsuttona@gmail.com>
2024-07-31 18:58:16 -0700

    updated popular items results

--- controllers/api/cryptoRoutes.js
+++ controllers/api/cryptoRoutes.js
@@ -1,5 +1,17 @@
 const { fetchCryptoData, fetchCurrencyData } = require('./coinApiRoutes') 
+const express = require('express');
+const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const itemsData = await fetchCryptoData(req.query.symbol)
+        res.json(itemsData[0])
+    } catch (error) {
+        console.error("Error fetching popular items:", error);
+        res.status(500).json({ error: "Failed to fetch popular items" });
+    }
+});
 
+module.exports = router;
 
 
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,5 +2,5 @@ module.exports = {
     formatDate: (date) => {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
-    },
+    }
   };
